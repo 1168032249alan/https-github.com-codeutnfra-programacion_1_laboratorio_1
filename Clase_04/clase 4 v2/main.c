fe226c630301567a7de859f5ae51545e0f344093
1168032249alan <36265104+1168032249alan@users.noreply.github.com>
2018-04-06 12:11:09 -0300

    clase 4
    clase 4

---
+++ Clase_04/clase 4 v2/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+//prototipo 1
+//void sumar1 (void);
+//prototipo 2
+//void sumar2(int, int);
+//prototipo 3
+//void sumar3();
+// prototipo que sirve
+void sumar4
+
+int main()
+{
+    //sumar1();
+    //sumar2(4, 9);
+
+    //int resultado ;
+    //resultado =sumar3();
+    //return 0
+}
+
+//void sumar1(void);
+//{
+//   int numero1;
+//   int numero2;
+//  int suma;
+
+   //numero1= pedirEntero("ingrese un numero ", 1, 1000);
+   //numero2= pedirEntero("ingrese un numero ", 1, 1000);
+   //suma= numero1 + numero2;
+
+   //printf("suma :%d" )
+//}
+//void sumar2(int n1, int n2)
+//{
+
+//}
